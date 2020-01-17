37c5fba0827e0a79a22b3cc63b4f6f58b068edbf
Lucien George <luciengeorge95@gmail.com>
2020-01-17 11:19:53 +0000

    modifying original data

--- python-exercises/csv_test.py
+++ python-exercises/csv_test.py
@@ -1,17 +1,38 @@
+# import csv
+
+# data = []
+# with open ('./test_data.csv', 'r') as csv_file:
+#   csv_reader = csv.reader(csv_file, delimiter=",")
+
+#   for row in csv_reader:
+#     print(' '.join(row))
+#     row = list(filter(lambda x: x != '', row))
+#     data.append(row)
+# print(data)
+
+# with open('./test_data.csv', 'w') as csv_file:
+#   csv_writer = csv.writer(csv_file, delimiter=",")
+#   csv_writer.writerow(['primary_name', 'category', 'age'])
+#   for row in data:
+#     csv_writer.writerow(row)
+
 import csv
 
 data = []
+id = 1
 with open ('./test_data.csv', 'r') as csv_file:
   csv_reader = csv.reader(csv_file, delimiter=",")
-
+  next(csv_reader)
   for row in csv_reader:
-    print(' '.join(row))
-    row = list(filter(lambda x: x != '', row))
+    row.append(id)
     data.append(row)
-print(data)
+    modified_row = row.copy()
+    modified_row[0] = modified_row[0][:2] + '  ' + modified_row[0][2:]
+    data.append(modified_row)
+    id += 1
 
 with open('./test_data.csv', 'w') as csv_file:
   csv_writer = csv.writer(csv_file, delimiter=",")
-  csv_writer.writerow(['primary_name', 'category', 'age'])
+  csv_writer.writerow(['primary_name', 'category', 'id'])
   for row in data:
     csv_writer.writerow(row)
